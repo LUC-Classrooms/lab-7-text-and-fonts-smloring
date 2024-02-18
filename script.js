18e0a31e1da60d44b312f0672bad786cf3aa1de4
smloring <156940158+smloring@users.noreply.github.com>
2024-02-18 23:00:42 +0000

    lab 7 submitted

--- script.js
+++ script.js
@@ -4,14 +4,15 @@
 
 function setup() {
   createCanvas(640, 240);
-  textFont("Arial");
+  textFont("comic sans");
 }
 
 function draw() {
   background(200);
   textSize(16);
-  text("one small step for a man ...", 20, 60);
+  text("once upon a time...", 20, 60);
   textSize(32);
-  text("one giant leap for mankind.", 20, 160);
+  if (mouseIsPressed)
+  text("they were roommates", 20, 160);
 
 }
